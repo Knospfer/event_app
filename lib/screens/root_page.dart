4280e87947db1d30cda7878261da333806bc20bf
knospfer <berrueta.mattia@gmail.com>
2020-12-08 11:47:45 +0100

    feat(app) : notched nav bar

--- lib/screens/root_page.dart
+++ lib/screens/root_page.dart
@@ -11,6 +11,10 @@ class RootState extends State<RootPage> {
     Center(
       child: Text('Home Page!'),
     ),
+    //workaroung for spacing real tab buttons
+    Center(
+      child: Text('mock Page!'),
+    ),
     Center(
       child: Text('Settings Page!'),
     ),
@@ -19,28 +23,45 @@ class RootState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        body: pages.elementAt(_selectedIndex),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange[300],
+          onPressed: null,
+          elevation: 2.0,
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          clipBehavior: Clip.antiAlias,
+          notchMargin: 10.0,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.blue,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              //workaroung for spacing real tab buttons
+              BottomNavigationBarItem(label: '', icon: Icon(Icons.home)),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              )
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.white,
+            onTap: _onItemTapped,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          )
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.red,
-        onTap: _onItemTapped,
-      ),
-    );
+        ));
   }
 
   _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    //workaroung for spacing real tab buttons
+    if (index != 1) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   }
 }
