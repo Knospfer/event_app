1e25a57db6bb41064ba897115b6dfafc2918f7fa
knospfer <berrueta.mattia@gmail.com>
2020-12-12 10:54:41 +0100

    feat(app): image picker card

---
+++ lib/widget/image_picker_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImagePickerPath {
+  static String unsplash = 'assets/unsplash.png';
+  static String gallery = 'assets/gallery.png';
+}
+
+class ImagePickerTitle {
+  static String unsplash = 'Unsplash';
+  static String gallery = 'Gallery';
+}
+
+class ImagePickerCard extends StatelessWidget {
+  final String imagePath;
+  final String cardTitle;
+
+  ImagePickerCard({this.imagePath, this.cardTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      child: Container(
+        height: 120.0,
+        width: 120.0,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter),
+        ),
+        child: Stack(
+          children: [
+            Container(color: Color.fromRGBO(0, 0, 0, 0.44)),
+            Center(
+                child: Text(
+              cardTitle,
+              style: TextStyle(color: Colors.white),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
