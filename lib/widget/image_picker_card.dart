5d1fee1693f2433e092b5daadc79bcb7ef54c2ec
knospfer <berrueta.mattia@gmail.com>
2020-12-15 08:00:32 +0100

    feat(app): imape picker card onTap callback

--- lib/widget/image_picker_card.dart
+++ lib/widget/image_picker_card.dart
@@ -14,35 +14,38 @@ class ImagePickerTitle {
 class ImagePickerCard extends StatelessWidget {
   final String imagePath;
   final String cardTitle;
+  final Function onTap;
 
-  ImagePickerCard({this.imagePath, this.cardTitle});
+  ImagePickerCard({this.imagePath, this.cardTitle, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      clipBehavior: Clip.hardEdge,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20.0))),
-      child: Container(
-        height: 120.0,
-        width: 120.0,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(imagePath),
-              fit: BoxFit.cover,
-              alignment: Alignment.topCenter),
-        ),
-        child: Stack(
-          children: [
-            Container(color: Color.fromRGBO(0, 0, 0, 0.44)),
-            Center(
-                child: Text(
-              cardTitle,
-              style: TextStyle(color: Colors.white),
-            )),
-          ],
-        ),
-      ),
-    );
+        clipBehavior: Clip.hardEdge,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+        child: GestureDetector(
+          onTap: this.onTap,
+          child: Container(
+            height: 120.0,
+            width: 120.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter),
+            ),
+            child: Stack(
+              children: [
+                Container(color: Color.fromRGBO(0, 0, 0, 0.44)),
+                Center(
+                    child: Text(
+                  cardTitle,
+                  style: TextStyle(color: Colors.white),
+                )),
+              ],
+            ),
+          ),
+        ));
   }
 }
