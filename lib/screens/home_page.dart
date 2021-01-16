336e36dbb84a13bff51c93ba8019c1e838e0f088
knospfer <berrueta.mattia@gmail.com>
2021-01-16 09:00:16 +0100

    feat(app): home page linked to bloc

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:event_app/models/event.dart';
+import 'package:event_app/providers/event_provider.dart';
 import 'package:event_app/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,48 +8,60 @@ import '../widget/event_card.dart';
 
 class HomePage extends StatelessWidget {
   final today = new DateFormat.yMMMMEEEEd().format(DateTime.now());
-  final EventModel event = EventModel(
-      eventDate: DateTime(2021, 4, 12),
-      currentDate: DateTime.now(),
-      imagePath: 'https://picsum.photos/200/300',
-      name: 'Mock Event');
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                today.toUpperCase(),
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Events',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+    final eventBloc = EventProvider.of(context);
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.fromLTRB(20, 60, 20, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            today.toUpperCase(),
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        Padding(
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, RoutesName.detailPage);
-              },
-              child: EventCard(eventModel: event),
-            ))
-      ],
-    );
+          Text(
+            'Events',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          StreamBuilder(
+              stream: eventBloc.events,
+              builder: (context, AsyncSnapshot<List<EventModel>> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: Text("There's no event yet!"));
+                }
+                return ListView.builder(
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      final event = snapshot.data[index];
+                      return _tappableCard(context, event);
+                    });
+              })
+        ],
+      ),
+    ));
+  }
+
+  Widget _tappableCard(BuildContext context, EventModel eventModel) {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 30),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, RoutesName.detailPage);
+          },
+          child: EventCard(eventModel: eventModel),
+        ));
   }
 }
