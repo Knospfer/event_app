a39417cc16ec6017fbcaf64ce4a6654f23b19c07
knospfer <berrueta.mattia@gmail.com>
2021-01-17 09:03:28 +0100

    feat(app): init db

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:event_app/app.dart';
+import 'package:event_app/database/db_initializer.dart';
 import 'package:flutter/cupertino.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DbInitalizer.initDb();
   runApp(App());
 }
