adece42d3a1e037c019e716622a40c56ba09de59
knospfer <berrueta.mattia@gmail.com>
2021-02-26 07:03:08 +0100

    feat(app): root page

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:event_app/core/enums/routes.dart';
 import 'package:event_app/dipendency_injection.dart';
 import 'package:event_app/features/add_event/presentation/blocs/add_event_bloc.dart';
 import 'package:event_app/features/add_event/presentation/screens/add_page.dart';
+import 'package:event_app/features/root_navigation/presentation/screens/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,14 +17,13 @@ class App extends StatelessWidget {
 }
 
 Route _routes(RouteSettings settings) {
-  //  TODO sarà add page
-  if (settings.name == RoutesName.rootPage)
+  if (settings.name == RoutesName.addPage)
     return MaterialPageRoute(builder: (context) {
       return BlocProvider<AddEventBloc>(
         create: (_) => getIt<AddEventBloc>(),
         child: AddPage(),
       );
     });
-  //TODO verrà rimosso
-  return MaterialPageRoute(builder: (_) => Center());
+
+  return MaterialPageRoute(builder: (_) => RootPage());
 }
---
+++ lib/features/root_navigation/presentation/screens/root_page.dart
@@ -0,0 +1,67 @@
+import 'package:event_app/core/enums/routes.dart';
+import 'package:flutter/material.dart';
+
+class RootPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RootState();
+}
+
+class RootState extends State<RootPage> {
+  int _selectedIndex = 0;
+  final pages = [
+    Center(child: Text('home page!')),
+    //workaroung for spacing real tab buttons
+    Center(
+      child: Text('mock Page!'),
+    ),
+    Center(
+      child: Text('Settings Page!'),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages.elementAt(_selectedIndex),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          onPressed: () {
+            Navigator.pushNamed(context, RoutesName.addPage);
+          },
+          elevation: 2.0,
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          clipBehavior: Clip.antiAlias,
+          notchMargin: 10.0,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.grey,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              //workaroung for spacing real tab buttons
+              BottomNavigationBarItem(label: '', icon: Icon(Icons.home)),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              )
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.white,
+            onTap: _onItemTapped,
+          ),
+        ));
+  }
+
+  _onItemTapped(int index) {
+    //workaroung for spacing real tab buttons
+    if (index != 1) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+}
